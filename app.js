92193b155e7e42518d4d8408e6b6926af9d51611
Milo <zeroeclipse@live.com>
2022-03-04 20:59:35 -0800

    Learning core types.

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-console.log('code goes here!!!');
-function add(n1, n2) {
-    if (typeof n1 !== 'number' && typeof n2 !== 'number') {
-        throw new Error('Incorrect input');
-    }
-    return n1 + n2;
-}
-var number1 = 'x';
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+// console.log('code goes here!!!')
+// function add(n1: number, n2: number) {
+//     // if(typeof n1 !== 'number' && typeof n2 !== 'number'){
+//     //     throw new Error('Incorrect input')
+//     // }
+//     return n1 + n2
+// }
+// const number1 = 5
+// const number2 = 2.8
+// const result = add(number1, number2)
+// console.log(result)
+// const person: {
+//     name: string,
+//     age: number
+// } = {
+//     name: "Milo",
+//     age: 32
+// }
+// const person: {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: "Milo",
+//     age: 32,
+//     hobbies: ["videogames", 'reading'],
+//     role: [2, 'author']
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 1] = "ADMIN";
+    Role[Role["READ_ONLY"] = 2] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 3] = "AUTHOR";
+})(Role || (Role = {})); //assigns labels to numbers
+var person = {
+    name: "Milo",
+    age: 32,
+    hobbies: ["videogames", 'reading'],
+    role: Role.ADMIN
+};
+var favoriteActivities;
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,14 +1,49 @@
-console.log('code goes here!!!')
+// console.log('code goes here!!!')
 
-function add(n1: number, n2: number) {
-    // if(typeof n1 !== 'number' && typeof n2 !== 'number'){
-    //     throw new Error('Incorrect input')
-    // }
-    return n1 + n2
+// function add(n1: number, n2: number) {
+//     // if(typeof n1 !== 'number' && typeof n2 !== 'number'){
+//     //     throw new Error('Incorrect input')
+//     // }
+//     return n1 + n2
+// }
+
+// const number1 = 5
+// const number2 = 2.8
+
+// const result = add(number1, number2)
+// console.log(result)
+
+// const person: {
+//     name: string,
+//     age: number
+// } = {
+//     name: "Milo",
+//     age: 32
+// }
+
+
+// const person: {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: "Milo",
+//     age: 32,
+//     hobbies: ["videogames", 'reading'],
+//     role: [2, 'author']
+// }
+
+enum Role { ADMIN = 1, READ_ONLY, AUTHOR }//assigns labels to numbers
+
+
+const person = {
+    name: "Milo",
+    age: 32,
+    hobbies: ["videogames", 'reading'],
+    role: Role.ADMIN
 }
 
-const number1 = 5
-const number2 = 2.8
+let favoriteActivities: string[]
 
-const result = add(number1, number2)
-console.log(result)
+console.log(person.name)
